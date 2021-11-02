68a29bc63ae59443e106cd664a8a7944f3482e87
AgneG25 <44949532+AgneG25@users.noreply.github.com>
2021-11-02 18:07:41 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,56 +1,18 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author 56130
- */
-public class User {
+public class HelloWorld {
 
-    private  int​ id​ ;
-    private​ String firstName​;
-     private​ String lastName​;
-     private​ int age;
-    private String profession;
-    private ArrayList<User> children;
+    public static void main(String[] args) {
 
-    public User(int id, String firstName, String lastName, int age) {
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
 
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.profession = profession;
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
 
-    }
-
-    public String getChildName(User child) {
-        if (!this.children.contains(child)) {
-            new Exception("Invalid argument!");
-        } else {
-            String name = null;
-            if (child.getFirstName() != null) {
-                name = child.getFirstName();
-            }
-            if (name == "Harry") {
-                name.replace('r', 'p');
-            }
-            if (name != null || name.length() > 0) {
-                name.concat(child.getLastName());
-            }
-        }
-        return this.getChildName(child);
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
     }
 }
